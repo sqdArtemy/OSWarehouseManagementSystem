c707f430aa34bd7ab3ef4e315323726bcb6ec14b
Artyom Tashyan <squiman15@gmail.com>
2023-10-22 15:44:44 +0500

    Added alembic.ini to git ignore

--- Backend/env_loader.py
+++ Backend/env_loader.py
@@ -7,8 +7,8 @@ load_dotenv()
 # Loading environment variables
 db_url = os.getenv("DATABASE_URL")
 
-# Update alembic.ini file with the database url
-with open("alembic.ini", 'r') as file:
+# Create alembic.ini file with the database url
+with open("alembic_template", 'r') as file:
     content = file.read()
 
 content = content.replace("sqlalchemy.url = driver://user:pass@localhost/dbname", f"sqlalchemy.url = {db_url}")
