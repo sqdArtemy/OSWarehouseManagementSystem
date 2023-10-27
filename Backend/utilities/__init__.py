eef5c2a1f1311f67f30774470a649ec08b145192
Artyom Tashyan <squiman15@gmail.com>
2023-10-27 17:17:56 +0500

    Created functions for decoding and encoding tokens.

--- Backend/utilities/__init__.py
+++ Backend/utilities/__init__.py
@@ -1,2 +1,2 @@
-from .functions import hash_password, check_password
+from .functions import hash_password, check_password, create_token, decode_token
 from .validators import is_email_valid, is_phone_valid
--- Backend/utilities/functions.py
+++ Backend/utilities/functions.py
@@ -12,3 +12,32 @@ def hash_password(password: str) -> bytes:
 def check_password(hashed_password: bytes, input_password: str) -> bool:
     # Checking password
     return bcrypt.checkpw(input_password.encode("utf-8"), hashed_password)
+
+
+def create_token(user_id: int, user_role: str) -> str:
+    """
+    Creates token with which user can be recognized by the server.
+    :param user_id: id of the user
+    :param user_role: role of the user
+    :return: token
+    """
+    role_identifier = {
+        "owner": "0",
+        "manager": "1",
+        "shipper": "2",
+        "customer": "3"
+    }
+
+    return role_identifier.get(user_role) + str(user_id*69 + 420)
+
+
+def decode_token(token: str) -> int:
+    """
+    Decodes token and returns user id.
+    :param token: encoded token.
+    :return: id of the user from database
+    """
+
+    user_id = (int(token[1:]) - 420) // 69
+
+    return user_id
