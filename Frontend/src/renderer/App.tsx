51060d6e79454e944fd2816e5018521646883456
JohnnyKoshev <komilzonu5@gmail.com>
2023-10-29 23:02:45 +0500

    feat: sign-in component: html + scss

--- Frontend/src/renderer/App.tsx
+++ Frontend/src/renderer/App.tsx
@@ -1,7 +1,7 @@
 import {MemoryRouter as Router, Routes, Route, Navigate} from 'react-router-dom';
 import {SignIn} from "./components/sign-in-component/sign-in";
 import './App.css';
-
+import './normalize.css';
 
 export default function App() {
   return (
