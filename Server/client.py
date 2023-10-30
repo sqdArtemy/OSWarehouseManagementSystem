970b5311fc4289a040428b6d33f6a432fe288c28
Saidjon Khaydar-Zade <87021841+reddias@users.noreply.github.com>
2023-10-30 22:39:58 +0500

    Updated the client.py file to connect one backend server

--- Server/client.py
+++ Server/client.py
@@ -1,8 +1,9 @@
 import socket
 import json
+import sys
 
 SERVER_IP = "127.0.0.1"  # Replace with the actual server's IP address
-PORT = 8080
+PORT = 8000
 
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
@@ -11,18 +12,22 @@ try:
     print("Connected to the server.")
 
     # Specify the role in a JSON message
-    #message = {
-     #   "role": "backend",
-      #  "content": "Hello from the Python client"
-    #}
-    #client_socket.send(json.dumps(message).encode())
-
+    message = {
+        "role": "backend",
+        "content": "Hello from the Python client"
+    }
+    client_socket.send(json.dumps(message).encode())
+    
     while True:
+        data = client_socket.recv(1024)
+        print("Server's response:", data.decode())
+        if(data.decode() == "There's already a connected backend"):
+            sys.exit(0)
+        
         new_message = input("Enter a message (or 'quit' to exit): ")
         client_socket.send(new_message.encode())
 
-        data = client_socket.recv(1024)
-        print("Server's response:", data.decode())
+        
 
 except ConnectionRefusedError:
     print("Connection to the server failed. Make sure the server is running.")
