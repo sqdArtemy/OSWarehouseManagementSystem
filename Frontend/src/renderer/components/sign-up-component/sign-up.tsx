45cd3e691c18975941a287c70e1ee4c57a767cf5
oneman13 <93471551+oneman13@users.noreply.github.com>
2023-11-09 00:11:54 +0500

    added sign-in first page

--- Frontend/src/renderer/components/sign-up-component/sign-up.tsx
+++ Frontend/src/renderer/components/sign-up-component/sign-up.tsx
@@ -1,24 +1,3 @@
-<<<<<<< Updated upstream
-export default function SignUp() {
-  return (
-    <div>
-      <h1>Sign Up</h1>
-      <form>
-        <input
-          type="text"
-          id="username"
-          name="username"
-          placeholder="Enter ID"
-        />
-        <input
-          type="password"
-          id="password"
-          name="password"
-          placeholder="Password"
-        />
-        <button type="submit">SIGN IN</button>
-      </form>
-=======
 import React from 'react';
 import './sign-up.scss';
 
@@ -56,7 +35,6 @@ export function SignUp() {
         </div>
 
       </div>
->>>>>>> Stashed changes
     </div>
   );
 }
