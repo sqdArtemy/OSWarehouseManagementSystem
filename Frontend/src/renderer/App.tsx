d2602cfbfd4dd5b6f49cc72c8be8dd4a8e82e07a
oneman13 <93471551+oneman13@users.noreply.github.com>
2023-11-09 00:32:10 +0500

    Update App.tsx

--- Frontend/src/renderer/App.tsx
+++ Frontend/src/renderer/App.tsx
@@ -1,14 +1,4 @@
-<<<<<<< Updated upstream
-import {
-  MemoryRouter as Router,
-  Routes,
-  Route,
-  Navigate,
-} from 'react-router-dom';
-import SignUp from './components/sign-up-component/sign-up';
-=======
 import {MemoryRouter as Router, Routes, Route, Navigate} from 'react-router-dom';
->>>>>>> Stashed changes
 import './App.css';
 import './normalize.css';
 import { SignUp } from './components/sign-up-component/sign-up';
@@ -17,13 +7,8 @@ export default function App() {
   return (
     <Router>
       <Routes>
-<<<<<<< Updated upstream
-        <Route path="/" element={<Navigate to="/sign-up" replace />} />
-        <Route path="/sign-up" element={<SignUp />} />
-=======
         <Route path="/" element={<Navigate to="/sign-up" replace/>}/>
         <Route path="/sign-up" element={<SignUp/>}/>
->>>>>>> Stashed changes
       </Routes>
     </Router>
   );
