ff081f112bfdc88b6925d7a7f050d99f7610c399
oneman13 <93471551+oneman13@users.noreply.github.com>
2023-11-09 00:08:37 +0500

    added sign-in first page

--- Frontend/src/renderer/App.tsx
+++ Frontend/src/renderer/App.tsx
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 import {
   MemoryRouter as Router,
   Routes,
@@ -5,15 +6,24 @@ import {
   Navigate,
 } from 'react-router-dom';
 import SignUp from './components/sign-up-component/sign-up';
+=======
+import {MemoryRouter as Router, Routes, Route, Navigate} from 'react-router-dom';
+>>>>>>> Stashed changes
 import './App.css';
 import './normalize.css';
+import { SignUp } from './components/sign-up-component/sign-up';
 
 export default function App() {
   return (
     <Router>
       <Routes>
+<<<<<<< Updated upstream
         <Route path="/" element={<Navigate to="/sign-up" replace />} />
         <Route path="/sign-up" element={<SignUp />} />
+=======
+        <Route path="/" element={<Navigate to="/sign-up" replace/>}/>
+        <Route path="/sign-up" element={<SignUp/>}/>
+>>>>>>> Stashed changes
       </Routes>
     </Router>
   );
--- Frontend/src/renderer/components/sign-up-component/sign-up.tsx
+++ Frontend/src/renderer/components/sign-up-component/sign-up.tsx
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 export default function SignUp() {
   return (
     <div>
@@ -17,6 +18,45 @@ export default function SignUp() {
         />
         <button type="submit">SIGN IN</button>
       </form>
+=======
+import React from 'react';
+import './sign-up.scss';
+
+export function SignUp() {
+  return (
+    <div className='sign-in-container'>
+      <header>WAREHOUSE MANAGEMENT SYSTEM</header>
+      <div className='content'>
+
+        <div className='status-icons'>
+          <div className='status-icon' id='sh'>SH</div>
+          <div className='status-icon' id='mng'>MNG</div>
+          <div className='status-icon' id='str'>STR</div>
+        </div>
+
+        <div className='login-form'>
+
+          <div className='tabs-container'>
+            <div className='tabs-left'>
+              <span id='id'>ID</span>
+              <span id='sign-in'>Sign Up</span>
+            </div>
+            <div className='tabs-right'>
+              <span className='disabled' id='login'>Login</span>
+              <span id='sign-up'>Sign Up</span>
+            </div>
+          </div>
+
+          <form>
+            <input id='email' placeholder={'Name'} />
+            <input type='Email' id='password' placeholder={'Email'} />
+            <button type='submit'>Continue</button>
+          </form>
+
+        </div>
+
+      </div>
+>>>>>>> Stashed changes
     </div>
   );
 }
