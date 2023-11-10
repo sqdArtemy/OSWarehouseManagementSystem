772d464de328b4bff03fc0592083a4518636681e
Artyom Tashyan <squiman15@gmail.com>
2023-11-11 02:28:26 +0500

    Minor fixes in models. Added triggers for updating capacity.

--- Backend/models/product.py
+++ Backend/models/product.py
@@ -1,12 +1,13 @@
 from sqlalchemy.orm import relationship
-from sqlalchemy import Column, Integer, String, Numeric, CheckConstraint
-from db_config import Base
+from sqlalchemy import Column, Integer, String, Numeric, CheckConstraint, ForeignKey
+from db_config import Base, SessionMaker
 
 
 class Product(Base):
     __tablename__ = "products"
 
     product_id = Column(Integer, primary_key=True, index=True, autoincrement=True)
+    company_id = Column(Integer, ForeignKey("companies.company_id"))
     product_name = Column(String(100), index=True, nullable=False)
     description = Column(String(255), index=True)
     weight = Column(Numeric(precision=20, scale=4))
@@ -16,6 +17,7 @@ class Product(Base):
 
     # Relationships with other tables
     inventory = relationship("Inventory", back_populates="product")
+    company = relationship("Company", back_populates="products")
     ordered_item = relationship("OrderItem", back_populates="product")
     product_transaction_items = relationship("TransactionItem", back_populates="product")
 
@@ -27,8 +29,10 @@ class Product(Base):
     )
 
     def to_dict(self):
+        product = SessionMaker().query(Product).filter(Product.product_id == self.product_id).first()
         return {
             "product_id": self.product_id,
+            "company": product.company.to_dict(),
             "product_name": self.product_name,
             "description": self.description,
             "weight": self.weight,
