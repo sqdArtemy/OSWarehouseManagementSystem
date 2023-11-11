eb5671958aac31fc805317df7a875a139f1d4e4d
JohnnyKoshev <komilzonu5@gmail.com>
2023-11-11 23:22:50 +0500

    feat: native node modules integration for renderer process

--- Frontend/src/main/main.ts
+++ Frontend/src/main/main.ts
@@ -75,9 +75,6 @@ const createWindow = async () => {
     height: 728,
     icon: getAssetPath('icon.png'),
     webPreferences: {
-      preload: app.isPackaged
-        ? path.join(__dirname, 'preload.js')
-        : path.join(__dirname, '../../.erb/dll/preload.js'),
       contextIsolation: false,
       nodeIntegration: true,
     },
