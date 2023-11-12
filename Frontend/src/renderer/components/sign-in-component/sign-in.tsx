47cddee35987168425c81edf14b8abb056bfcf13
RavshanMVP <93672457+RavshanMVP@users.noreply.github.com>
2023-11-12 19:20:36 +0500

    feat: call ipc in the frontend

--- Frontend/src/renderer/components/sign-in-component/sign-in.tsx
+++ Frontend/src/renderer/components/sign-in-component/sign-in.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './sign-in.scss';
 import { useNavigate } from 'react-router-dom';
+import { apiClient } from '../../index';
 
 export function SignIn() {
   const navigate = useNavigate();
@@ -28,8 +29,15 @@ export function SignIn() {
             <input type="password" id="password" placeholder={'Password'} />
             <button
               type="submit"
-              onClick={() => {
+              onClick={async () => {
                 navigate('/owner');
+                // const testMessage = await apiClient.send(
+                //   {"url": "/user/register",
+                //      "method": "POST",
+                //      "body": {"company_name": "test", "company_address": "test", "company_email": "tets@gmail.com", "user_name": "TestUser", "user_surname": "TestSurname", "user_email": "test@user.mail", "user_role": "owner", "user_phone": "+9989778284", "password": "test123", "confirm_password": "test123"},
+                //      headers: {}
+                //   });
+                // console.log(testMessage);
               }}
             >
               SIGN IN
--- Frontend/src/renderer/index.tsx
+++ Frontend/src/renderer/index.tsx
@@ -1,9 +1,24 @@
 import { createRoot } from 'react-dom/client';
 import App from './App';
+import { TcpClient } from './services/tcpClient';
+import dotenv from "dotenv";
 
 const container = document.getElementById('root') as HTMLElement;
 const root = createRoot(container);
 root.render(<App />);
+
+dotenv.config({ path: ".env" });
+const serverAddress = process.env.IP_IPC ?? '127.0.0.1';  // Change this to your server's IP or hostname
+const serverPort = process.env.PORT_IPC ?? 8000;
+export const apiClient = new TcpClient(serverAddress, Number(serverPort));
+
+(async ()=> {
+  try {
+    await apiClient.connect();
+  } catch (e) {
+    console.log(e);
+  }
+})();
 
 // // calling IPC exposed from preload script
 // window.electron.ipcRenderer.once('ipc-example', (arg) => {
