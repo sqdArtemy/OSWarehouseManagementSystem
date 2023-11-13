1fdb2d416f1bd60abc2760b63350f3f8882cfb7c
JohnnyKoshev <komilzonu5@gmail.com>
2023-11-13 23:47:42 +0500

    feat: owner-dashboard-users component: event handlers, html + scss

--- Frontend/src/renderer/index.tsx
+++ Frontend/src/renderer/index.tsx
@@ -1,19 +1,19 @@
 import { createRoot } from 'react-dom/client';
 import App from './App';
 import { TcpClient } from './services/tcpClient';
-import dotenv from "dotenv";
+import dotenv from 'dotenv';
 import { UserApi } from './services/api/userApi';
 
 const container = document.getElementById('root') as HTMLElement;
 const root = createRoot(container);
 root.render(<App />);
 
-dotenv.config({ path: ".env" });
+dotenv.config({ path: '.env' });
 const serverAddress = process.env.IP_IPC ?? '127.0.0.1';  // Change this to your server's IP or hostname
 const serverPort = process.env.PORT_IPC ?? 8000;
 export const apiClient = new TcpClient(serverAddress, Number(serverPort));
 
-(async ()=> {
+(async () => {
   try {
     //await apiClient.connect();
   } catch (e) {
