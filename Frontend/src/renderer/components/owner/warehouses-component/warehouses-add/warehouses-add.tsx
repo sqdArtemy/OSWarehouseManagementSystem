354a0bb5fddbe9bd0772e7689716c5940333d3e4
oneman13 <93471551+oneman13@users.noreply.github.com>
2023-11-13 21:47:39 +0500

    finished page "add warehouse"

--- Frontend/src/renderer/components/owner/warehouses-component/warehouses-add/warehouses-add.tsx
+++ Frontend/src/renderer/components/owner/warehouses-component/warehouses-add/warehouses-add.tsx
@@ -4,12 +4,31 @@ import './warehouses-add.scss';
 export default function WarehousesAdd() {
   return (
     <div className="warehouses-container">
-      <span className="label" id="label">ADD WAREHOUSE</span>
+      <span id="label">ADD WAREHOUSE</span>
       <div className="add-warehouse-form">
-        <span className="">Warehouse Name</span>
-        <span className="">Warehouse Address</span>
-        <span className="">Manager</span>
-        <span className="">Overall Warehouse Capacity</span>
+        <div className="titles">
+          <span className="">Warehouse Name:</span>
+          <span className="">Warehouse Address:</span>
+          <span className="">Manager:</span>
+          <span className="">Overall Warehouse Capacity:</span>
+        </div>
+        <div className="inputs">
+          <input />
+          <input />
+          <select name="managers" id="managers">
+            <option value="rigatoni">Rigatoni</option>
+            <option value="dave">Dave</option>
+            <option value="pumpernickel">Pumpernickel</option>
+            <option value="reeses">Reeses</option>
+          </select>
+
+
+          <input />
+          <div className="buttons">
+            <button id="clear">CLEAR</button>
+            <button id="submit">SUBMIT</button>
+          </div>
+        </div>
       </div>
     </div>
   );
