30fdc47c848ffb69ff621b87250a4800e7a3e7af
oneman13 <93471551+oneman13@users.noreply.github.com>
2023-11-16 12:20:51 +0500

    changed css of account

--- Frontend/src/renderer/components/owner/account-component/owner-account.tsx
+++ Frontend/src/renderer/components/owner/account-component/owner-account.tsx
@@ -11,27 +11,27 @@ export default function OwnerAccount() {
     </div>
         <div className="owner-account-form">
           <div className="titles">
-            <span className="">First Name: </span>
-            <span className="">Last Name:</span>
-            <span className="">Email:</span>
-            <span className="">Phone:</span>
-            {!changePassDisplay ? ( <span className="">Password:</span>) : (<><span> Current Password: </span><span>New Password:</span><span>Confirm Password:</span></>)}
+            <span className="span">First Name: </span>
+            <span className="span">Last Name:</span>
+            <span className="span">Email:</span>
+            <span className="span">Phone:</span>
+            {!changePassDisplay ? ( <span className="span">Password:</span>) : (<><span className="span"> Current Password: </span><span className="span">New Password:</span><span className="span">Confirm Password:</span></>)}
           </div>
           <div className="inputs">
-            <input className="first-name"/>
-            <input className="last-name"/>
-            <input type="email"className="email"/>
-            <input type="phone" className="phone"/>
-            {!changePassDisplay ? (<input type="password" />) : (<><input type="password" /><input type="password" /><input type="password" /></>)}
+            <input className="input" id="first-name"/>
+            <input className="input" id="last-name"/>
+            <input type="email" className="input" id="email"/>
+            <input type="phone" className="input" id="phone"/>
+            {!changePassDisplay ? (<input type="password" className="input"/>) : (<><input type="password" className="input"/><input type="password" className="input"/><input type="password" className="input"/></>)}
             <div className="buttons">
-              <button id="change-pass"
+              <button  className="button" id="change-pass"
                       onClick={
                         ()=>setChangePassDisplay(true)
                         }>
                         Change Password
               </button>
-              <button id="reset">Reset</button>
-              <button id="save">Save</button>
+              <button className="button" id="reset">Reset</button>
+              <button className="button" id="save">Save</button>
             </div>
           </div>
         </div>
