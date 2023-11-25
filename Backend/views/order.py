38c4e00e5a29becf742a84d9896441dc74b7e1ad
Artyom Tashyan <squiman15@gmail.com>
2023-11-25 15:20:11 +0500

    9th step from OCL

--- Backend/views/order.py
+++ Backend/views/order.py
@@ -3,7 +3,7 @@ from datetime import datetime
 from sqlalchemy import func
 
 from db_config import get_session
-from models import Order, Transport, OrderItem, Product
+from models import Order, Transport, OrderItem, Product, Vendor, Warehouse
 from services import view_function_middleware, check_allowed_methods_middleware
 from services.generics import GenericView
 from utilities import ValidationError, is_instance_already_exists
@@ -14,6 +14,59 @@ from utilities.enums.method import Method
 class OrderView(GenericView):
     model = Order
     model_name = "order"
+
+    @view_function_middleware
+    @check_allowed_methods_middleware([Method.GET.value])
+    def get(self, request: dict) -> dict:
+        """
+        Get an order from the database.
+        :param request: dictionary containing url, method, body and headers
+        :return: dictionary containing status_code and response body
+        """
+        requester_role = self.requester_role
+        requester_id = self.requester_id
+        order = self.instance
+        requester_warehouses = []
+        requester_vendors = []
+
+        if order is None:
+            raise ValidationError(f"{self.model_name.capitalize()} with given id does not exist.", 404)
+
+        with get_session() as session:
+            if requester_role == UserRole.VENDOR.value["code"]:
+                requester_vendors = session.query(Vendor.vendor_id).filter_by(vendor_owner_id=requester_id).all()
+                requester_vendors = [vendor[0] for vendor in requester_vendors]
+            elif requester_role in (UserRole.MANAGER.value["code"], UserRole.SUPERVISOR.value["code"]):
+                requester_warehouses = session.query(Warehouse.warehouse_id).filter_by(supervisor_id=requester_id).all()
+                requester_warehouses = [warehouse[0] for warehouse in requester_warehouses]
+
+            if requester_role != UserRole.ADMIN.value["code"] and (
+                    (
+                    requester_role == UserRole.VENDOR.value["code"] and (
+                    (order.order_type == "from_warehouse" and order.supplier_id not in requester_vendors) or
+                    (order.order_type == "to_warehouse" and order.recipient_id not in requester_vendors)
+                       )
+                    ) or (
+                    (requester_role in (UserRole.MANAGER.value["code"], UserRole.SUPERVISOR.value["code"])) and (
+                        (order.order_type == "to_warehouse" and order.recipient_id not in requester_warehouses) or
+                        (order.order_type == "from_warehouse" and order.supplier_id not in requester_warehouses)
+                       )
+                    )
+            ):
+                raise ValidationError("You are not allowed to see this order.", 403)
+
+            total_volume = session.query(func.sum(OrderItem.quantity * Product.volume)). \
+                join(Product, OrderItem.product_id == Product.product_id). \
+                filter(OrderItem.order_id == order.order_id).scalar()
+
+            self.response.status_code = 200
+            self.response.data = order.to_dict(cascade_fields=())
+            self.response.data["total_volume"] = total_volume
+            self.response.data["order_items"] = [
+                order_item.to_dict(cascade_fields=()) for order_item in order.ordered_items
+            ]
+
+            return self.response.create_response()
 
     @view_function_middleware
     @check_allowed_methods_middleware([Method.POST.value])
@@ -55,7 +108,8 @@ class OrderView(GenericView):
             raise ValidationError(f"Transport with id {transport_id} does not exist.", 404)
 
         with get_session() as session:
-            transport_capacity = session.query(Transport.transport_capacity).filter_by(transport_id=transport_id).first()[0]
+            transport_capacity = \
+            session.query(Transport.transport_capacity).filter_by(transport_id=transport_id).first()[0]
             order_capacity = session.query(func.sum(OrderItem.quantity * Product.volume)). \
                 join(Product, OrderItem.product_id == Product.product_id). \
                 filter(OrderItem.order_id == order.order_id).scalar()
