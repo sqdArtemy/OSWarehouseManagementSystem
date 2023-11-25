63bcbe8abfc6fe85d421766d1de10b3d7f8c24a4
RavshanMVP <93672457+RavshanMVP@users.noreply.github.com>
2023-11-25 21:30:54 +0500

    add vendors component

--- Frontend/src/renderer/index.tsx
+++ Frontend/src/renderer/index.tsx
@@ -5,6 +5,7 @@ import dotenv from 'dotenv';
 import { UserApi } from './services/api/userApi';
 import { ProductApi } from './services/api/productApi';
 import { WarehouseApi } from './services/api/warehouseApi';
+import { VendorApi } from './services/api/vendorApi';
 
 const container = document.getElementById('root') as HTMLElement;
 const root = createRoot(container);
@@ -26,6 +27,7 @@ export const apiClient = new TcpClient(serverAddress, Number(serverPort));
 export const userApi = new UserApi();
 export const productApi = new ProductApi();
 export const warehouseApi = new WarehouseApi();
+export const vendorApi = new VendorApi();
 // // calling IPC exposed from preload script
 // window.electron.ipcRenderer.once('ipc-example', (arg) => {
 //   // eslint-disable-next-line no-console
