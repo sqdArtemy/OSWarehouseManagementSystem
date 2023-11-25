60ea98eb6909ce197e37eafaa3cf04435c53605a
Artyom Tashyan <squiman15@gmail.com>
2023-11-25 20:39:01 +0500

    5th step from OLC

--- Backend/views/order.py
+++ Backend/views/order.py
@@ -15,6 +15,66 @@ class OrderView(GenericView):
     model = Order
     model_name = "order"
 
+    @staticmethod
+    def __get_remaining_volume(warehouse_id: int) -> float:
+        """
+        Get remaining volume of a warehouse.
+        """
+        with get_session() as session:
+            remaining_volume = (
+                session.query(
+                    Warehouse.remaining_capacity -
+                    func.sum(OrderItem.quantity * Product.volume)
+                )
+                .join(Order, Order.recipient_id == Warehouse.warehouse_id)
+                .join(OrderItem, OrderItem.order_id == Order.order_id)
+                .join(Product, Product.product_id == OrderItem.product_id)
+                .filter(Order.status.in_(["processing", "delivered", "submitted"]))
+                .filter(Warehouse.warehouse_id == warehouse_id)
+                .group_by(Warehouse.warehouse_id)
+            ).scalar()
+
+            return remaining_volume
+
+    @staticmethod
+    def __get_remaining_products(warehouse_id: int) -> list[tuple]:
+        """
+        Get remaining products of a warehouse.
+        """
+        with get_session() as session:
+            remaining_products = (
+                session.query(
+                    Inventory.product_id,
+                    func.sum(Inventory.quantity).label('remaining_quantity')
+                )
+                .join(Rack, Inventory.rack_id == Rack.rack_id)
+                .filter(Rack.warehouse_id == warehouse_id)
+                .group_by(Inventory.product_id)
+            ).all()
+
+            return remaining_products
+
+    def __is_warehouse_capacity_enough(self, warehouse_id: int, total_volume: float) -> bool:
+        """
+        Check if warehouse capacity is enough.
+        """
+        remaining_volume = self.__get_remaining_volume(warehouse_id)
+        return remaining_volume >= total_volume
+
+    def __are_warehouse_products_enough(self, warehouse_id: int, products_to_order: dict) -> bool:
+        """
+        Check if warehouse products are enough.
+        """
+        remaining_products = self.__get_remaining_products(warehouse_id)
+
+        for product_id, quantity in products_to_order.items():
+            for remaining_product in remaining_products:
+                if product_id == remaining_product[0]:
+                    if quantity > remaining_product[1]:
+                        return False
+
+        return True
+
     @view_function_middleware
     @check_allowed_methods_middleware([Method.GET.value])
     def get(self, request: dict) -> dict:
@@ -163,36 +223,11 @@ class OrderView(GenericView):
                     raise ValidationError("All products must be of the same type", 400)
 
             if order_type == "to_warehouse":
-                remaining_volume = (
-                    session.query(
-                        Warehouse.remaining_capacity -
-                        func.sum(OrderItem.quantity * Product.volume)
-                    )
-                    .join(Order, Order.recipient_id == Warehouse.warehouse_id)
-                    .join(OrderItem, OrderItem.order_id == Order.order_id)
-                    .join(Product, Product.product_id == OrderItem.product_id)
-                    .filter(Order.status.in_(["processing", "delivered", "submitted"]))
-                    .filter(Warehouse.warehouse_id == warehouse_id)
-                    .group_by(Warehouse.warehouse_id)
-                ).scalar()
-                if remaining_volume < total_volume:
+                if not self.__is_warehouse_capacity_enough(warehouse_id, total_volume):
                     raise ValidationError("Warehouse capacity is not enough.", 400)
             elif order_type == "from_warehouse":
-                remaining_products = (
-                    session.query(
-                        Inventory.product_id,
-                        func.sum(Inventory.quantity).label('remaining_quantity')
-                    )
-                    .join(Rack, Inventory.rack_id == Rack.rack_id)
-                    .filter(Rack.warehouse_id == warehouse_id)
-                    .group_by(Inventory.product_id)
-                ).all()
-
-                for product_id, quantity in products_to_order.items():
-                    for remaining_product in remaining_products:
-                        if product_id == remaining_product[0]:
-                            if quantity > remaining_product[1]:
-                                raise ValidationError("Warehouse does not have enough products.", 400)
+                if not self.__are_warehouse_products_enough(warehouse_id, products_to_order):
+                    raise ValidationError("Warehouse does not have enough products.", 400)
 
             order = Order(
                 supplier_id=vendor_id if order_type == "to_warehouse" else warehouse_id,
@@ -231,10 +266,92 @@ class OrderView(GenericView):
         :param request: dictionary containing url, method, body and headers
         :return: dictionary containing status_code and response body
         """
-        if self.requester_role == UserRole.ADMIN.value["code"]:
-            return super().update(request=request)
 
-        return super().update(request=request)
+        order_items = self.body.get("items")
+        vendor_id = self.body.get("vendor_id")
+        requester_role = self.requester_role
+        order = self.instance
+        warehouse_id = order.supplier_id if order.order_type == "from_warehouse" else order.recipient_id
+        old_items = order.ordered_items
+        total_volume = 0
+        total_price = 0
+        products_to_order = {}
+        product_ids = []
+
+        if order is None:
+            raise ValidationError(f"{self.model_name.capitalize()} with given id does not exist.", 404)
+
+        if requester_role not in (UserRole.ADMIN.value["code"], UserRole.VENDOR.value["code"]):
+            raise ValidationError("Only vendor can update an order.", 403)
+
+        if order.order_status != "new":
+            raise ValidationError(f"You can not update order in status '{order.order_status}'", 403)
+
+        if (order.order_type == "to_warehouse" and order.supplier_id != vendor_id) or (
+                order.order_type == "from_warehouse" and order.recipient_id != vendor_id):
+            raise ValidationError("Order not found", 404)
+
+        if len(order_items) == 0:
+            raise ValidationError("Order must contain at least one item.", 400)
+
+        with get_session() as session:
+            vendor = session.query(Vendor).filter_by(vendor_id=vendor_id).first()
+            main_product_type = session.query(Product.product_type). \
+                filter_by(product_id=order_items[0]["product_id"]).scalar()
+
+            for item in order_items:
+                product_id, quantity = item.get("product_id"), item.get("quantity")
+
+                if not is_instance_already_exists(Product, product_id=product_id):
+                    raise ValidationError(f"Product with id {product_id} does not exist", 404)
+
+                product = session.query(Product).filter_by(product_id=product_id).first()
+                total_volume += product.volume * quantity
+                products_to_order[product_id] = quantity
+                product_ids.append(product_id)
+
+                if not vendor.is_government:
+                    total_price += product.price * quantity
+
+                if product.product_type != main_product_type:
+                    raise ValidationError("All products must be of the same type", 400)
+
+            if order.order_type == "to_warehouse":
+                if not self.__is_warehouse_capacity_enough(warehouse_id, total_volume):
+                    raise ValidationError("Warehouse capacity is not enough.", 400)
+            elif order.order_type == "from_warehouse":
+                if not self.__are_warehouse_products_enough(warehouse_id, products_to_order):
+                    raise ValidationError("Warehouse does not have enough products.", 400)
+
+            for old_item in old_items:
+                old_product_id = old_item.product_id
+                if old_product_id not in product_ids:
+                    session.delete(old_item)
+                else:
+                    old_item.quantity = products_to_order[old_product_id]
+                    products_to_order.pop(old_item.product_id)
+
+            if len(list(products_to_order.keys())) > 0:
+                for product_id, quantity in products_to_order.items():
+                    order_item = OrderItem(
+                        order_id=order.order_id,
+                        product_id=product_id,
+                        quantity=quantity
+                    )
+                    session.add(order_item)
+
+            order.total_price = total_price
+            order.updated_at = datetime.now()
+            session.commit()
+
+            self.response.status_code = 200
+            self.response.data = order.to_dict(cascade_fields=())
+            self.response.data["items"] = [
+                order_item.to_dict(cascade_fields=()) for order_item in order.ordered_items
+            ]
+            self.response.data["total_volume"] = total_volume
+
+            return self.response.create_response()
 
     @view_function_middleware
     @check_allowed_methods_middleware([Method.PUT.value])
@@ -277,7 +394,7 @@ class OrderView(GenericView):
     @check_allowed_methods_middleware([Method.PUT.value])
     def cancel(self, request: dict) -> dict:
         """
-        Change status of an order to cancelled in the database.
+        Change status of an order to "cancelled" in the database.
         :param request: dictionary containing url, method and body
         :return: dictionary containing status_code and response body
         """
