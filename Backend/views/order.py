f2a53b358278c29c83c25cb9fcabe5df4577b56d
asus <abduraufv23@gmail.com>
2023-12-03 13:25:39 +0500

    fix logic for lost and damaged in task 15 (order lifecycle)

--- Backend/views/order.py
+++ Backend/views/order.py
@@ -736,10 +736,24 @@ class OrderView(GenericView):
             warehouse_id = session.query(Warehouse.warehouse_id).filter_by(supervisor_id=creator_id).scalar()
             order = session.query(Order).filter_by(order_id=order_id, supplier_id=warehouse_id)
             if not order.first():
-                raise ValidationError("Order Not Found", 404)
+                raise ValidationError("Order Not Found.", 404)
+
+            if order.first().order_status in ("lost", "damaged"):
+                # change updated_at and order_status
+                order = order.first()
+                order.updated_at = datetime.now()
+                session.commit()
+
+                self.response.status_code = 200
+                self.response.data = order.to_dict(cascade_fields=())
+                return self.response.create_response()
 
             if order.first().order_status != "delivered":
-                raise ValidationError("You cannot receive orders that are not delivered", 400)
+                raise ValidationError("You cannot receive orders that are not delivered.", 400)
+
+            # if order_items is empty
+            if len(order.first().ordered_items) == 0:
+                raise ValidationError("No items left in the order.", 400)
 
             # main logic
             filled_inventories = self.body.get("filled_inventories")
